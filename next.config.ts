7ee5162b7dae45ac356e2525a3774fa591b2669b
Cecil <cecilmateo@hotmail.com>
2025-12-23 19:22:00 -0500

    Changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
+  const nextConfig = {
+  experimental: {
+    turbo: false,
+  },
+};
+module.exports = nextConfig;
 export default nextConfig;
